b0a00e170d89034f60bcabf3eb4bb93370f4bdaf
Konrad Hölzenbein <konrad@shop4fans.io>
2025-06-27 12:39:50 +0200

    Shipment Endpoints added

--- src/api/orders-api.ts
+++ src/api/orders-api.ts
@@ -1,44 +1,64 @@
 import { ORDERS_PATH } from "../endpoints/spod-endpoints.js";
-import { HttpClient } from "../http/http-client.js"
-import { CreateOrder, ErrorResponse, GetOrder } from "../types/index.js";
+import { HttpClient } from "../http/http-client.js";
+import {
+  CreateOrder,
+  ErrorResponse,
+  GetOrder,
+  GetShipmentsResponse,
+  GetShippingTypesResponse,
+} from "../types/index.js";
 
 export class OrdersApi {
-    constructor(private client: HttpClient){}
+  constructor(private client: HttpClient) {}
 
-    create(props: CreateOrder){
-        return this.client.request<GetOrder>(
-            "POST",
-            ORDERS_PATH,
-            props
-        );
-    }
+  create(props: CreateOrder) {
+    return this.client.request<GetOrder>("POST", ORDERS_PATH, props);
+  }
 
-    update(orderId: string, props: CreateOrder){
-        return this.client.request<GetOrder>(
-            "UPDATE",
-            `${ORDERS_PATH}/${orderId}`,
-            props
-        );
-    }
+  update(orderId: string, props: CreateOrder) {
+    return this.client.request<GetOrder>(
+      "UPDATE",
+      `${ORDERS_PATH}/${orderId}`,
+      props,
+    );
+  }
 
-    get(orderId: string){
-        return this.client.request<GetOrder>(
-            "GET",
-            `${ORDERS_PATH}/${orderId}`
-        );
-    }
+  get(orderId: string) {
+    return this.client.request<GetOrder>("GET", `${ORDERS_PATH}/${orderId}`);
+  }
 
-    confirm(orderId: string){
-        return this.client.request<ErrorResponse | void>(
-            "POST",
-            `${ORDERS_PATH}/${orderId}/confirm`
-        )
-    }
+  confirm(orderId: string) {
+    return this.client.request<ErrorResponse | void>(
+      "POST",
+      `${ORDERS_PATH}/${orderId}/confirm`,
+    );
+  }
 
-    cancel(orderId: string){
-        return this.client.request<ErrorResponse | void>(
-            "POST",
-            `${ORDERS_PATH}/${orderId}/cancel`
-        )
-    }
+  cancel(orderId: string) {
+    return this.client.request<ErrorResponse | void>(
+      "POST",
+      `${ORDERS_PATH}/${orderId}/cancel`,
+    );
+  }
+
+  get_available_shipping_types(orderId: string) {
+    return this.client.request<GetShippingTypesResponse>(
+      "GET",
+      `${ORDERS_PATH}/${orderId}/shippingTypes`,
+    );
+  }
+
+  set_shipping_type(orderId: string) {
+    return this.client.request<void>(
+      "POST",
+      `${ORDERS_PATH}/${orderId}/shippingType`,
+    );
+  }
+
+  get_shipments(orderId: string) {
+    return this.client.request<GetShipmentsResponse>(
+      "GET",
+      `${ORDERS_PATH}/${orderId}/shipments`,
+    );
+  }
 }
