f74185bef9ea91b71ededc1afd669368f9b726c8
Konrad Hölzenbein <konrad@shop4fans.io>
2025-06-27 13:06:28 +0200

    Subscription Endpoints added

--- src/spreadconnect.ts
+++ src/spreadconnect.ts
@@ -1,11 +1,17 @@
 import { HttpClient } from "./http/http-client.js";
 import { ArticlesApi } from "./api/articles-api.js";
+import { OrdersApi } from "./api/orders-api.js";
+import { SubscriptionsApi } from "./api/subscriptions-api.js";
 
 export class Spreadconnect {
-    public articles: ArticlesApi;
+  public articles: ArticlesApi;
+  public orders: OrdersApi;
+  public subscriptions: SubscriptionsApi;
 
-    constructor({baseUrl, token}: {baseUrl: string, token: string}) {
-        const client = new HttpClient(baseUrl, token)
-        this.articles = new ArticlesApi(client);
-    }
+  constructor({ baseUrl, token }: { baseUrl: string; token: string }) {
+    const client = new HttpClient(baseUrl, token);
+    this.articles = new ArticlesApi(client);
+    this.orders = new OrdersApi(client);
+    this.subscriptions = new SubscriptionsApi(client);
+  }
 }
